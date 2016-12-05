afbaead2f2ea8bdbd2ebf9cf67f550ffcbd06109
Rukhsar <rukhsar.man@gmail.com>
2016-12-05 16:20:04 +0000

    service provider added

---
+++ src/ActiveRouteServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Rukhsar\ActiveRoute;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+
+class ActiveRouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
